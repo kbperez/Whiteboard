7ba8fd8bf5e9134cd97a2110fdde7bc1d72aca1e
kbperez <kbpgperez@hotmail.com>
2018-02-23 08:48:48 -0800

    anagram sorter

--- wb29/lib/solution.js
+++ wb29/lib/solution.js
@@ -1,29 +1,32 @@
 'use strict';
 
-module.exports =  function (matrix) {
-  if (!matrix) return null;
-  if (!Array.isArray(matrix)) return 'Input must be an array';
-  //create empty array to hold manipulated elements
-  let temp = [];
-  //determine size of incoming matrix
-  let rows = matrix.length;
-  let columns = matrix[0].length;
+module.exports =  function (array) {
+  //validate input
+  if (!array) return null;
+  if (!Array.isArray(array)) return 'Input must be an array';
 
-  for(let i=0; i<columns; i++) {
-    for(let j=0; j<rows; j++) {
-      temp.push([]);
+  //create empty object to hold manipulated elements
+  let result = {};
+  let hashKey='';
+  let answer=[];
+
+  for(let i=0; i<array.length; i++) {
+
+    if (typeof array[i] !== 'string') return 'Array elements must be strings';
+
+    hashKey = array[i].split('').sort().join('');
+
+    result[hashKey] ? result[hashKey].push(array[i]) : result[hashKey] = [array[i]];
+
+    hashKey = '';
+
+  }
+
+  for(let property in result) {
+    if(result[property]) {
+      answer.push(result[property]);
     }
   }
 
-  //push into new array after performing callback
-  for(let i=0; i<rows; i++) {
-    for(let j=0; j<columns; j++) {
-      temp[j].push(matrix[i][j]);
-      // console.log('temp', temp);
-    }
-  }
-  matrix = temp;
-
-  //return original array in rotated form
-  return matrix;
+  return answer.reduce((a, b) => a.concat(b));
 };
