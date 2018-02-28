37e54aa9951ce5bb542ac0cdeefe3e51fe793889
kbperez <kbpgperez@hotmail.com>
2018-02-28 14:03:52 -0800

    function to sum digits of exponential calucation given base and exponent with tests

---
+++ wb33/__test__/solution.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const solution = require('../lib/solution');
+require('jest');
+
+describe('valid output type', () => {
+  it('should return a number', () => {
+    expect(solution(1,2)).toBeTruthy();
+    expect(typeof solution(1,2)).toBe('number');
+  });
+});
+
+describe('valid output', () => {
+  it('should return the correct answer', () => {
+    expect(solution(2,3)).toEqual(8);
+    expect(solution(-1,2)).toEqual(1);
+    expect(solution(2.5, 8.6)).toEqual(16);
+  });
+});
+
+describe('invalid input', () => {
+  it('should return null if missing input', () => {
+    expect(solution()).toBeNull;
+    expect(solution(1)).toBeNull;
+
+  });
+  it('should return a message if input is not a number', () => {
+    expect(solution(3, '2')).toEqual('Input must be 2 numbers');
+    expect(solution('three', 'two')).toEqual('Input must be 2 numbers');
+  });
+});
---
+++ wb33/index.js
@@ -0,0 +1,8 @@
+'use strict';
+const solution = require ('./lib/solution');
+
+
+console.log('standard', solution(2.5, 8.6));
+
+
+console.log('negative',solution(-1, 2));
